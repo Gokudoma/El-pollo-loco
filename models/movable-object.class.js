651bf67b2fcfcd5f21e8e0b3aaf3a6d053422c3d
Stephan <stephan_wacker@hotmail.com>
2025-06-10 14:41:44 +0200

    implemented Objects

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,20 +1,22 @@
 class MovableObject {
-        x = 120;
-        y = 400;
-        img;
+    x = 120;
+    y = 250;
+    img;
+    height= 150;
+    width= 100;
 
 
-        loadImage(path) {
-            this.img = new Image();
-            this.img.src = path;
-        }
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
 
 
-        moveRight() {
-            console.log('Moving right');
-        }
+    moveRight() {
+        console.log('Moving right');
+    }    
 
-        moveLeft() {
+    moveLeft() {
         
-        }
+    }
 }
