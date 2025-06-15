cda7f187f6d2ae55d56b117718fc76cf4486fbe1
Stephan <stephan_wacker@hotmail.com>
2025-06-15 15:59:41 +0200

    bug fixes

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,21 +1,25 @@
 class MovableObject extends DrawableObject {
-  speed = 0.15;
-  otherDirection = false;
-  speedY = 0;
-  acceleration = 2.5;
-  animationFrameSkip = 0;
-  animationSkipThreshold = 7;
+    speed = 0.15;
+    otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
+    animationFrameSkip = 0;
+    animationSkipThreshold = 7;
 
-  offset = {
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  };
+    offset = {
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    };
 
     applyGravity() {
-        setInterval(() => {
+        this.gravityInterval = setInterval(() => {
             if (this instanceof Character && this.isDead()) {
+                return;
+            }
+            if (this instanceof ThrowableObject && this.isSplashing) {
+                clearInterval(this.gravityInterval);
                 return;
             }
 
@@ -26,76 +30,76 @@ class MovableObject extends DrawableObject {
         }, 1000 / 25);
     }
 
-  isAboveGround() {
-    if (this instanceof ThrowableObject) {
-      return true;
-    } else {
-      return this.y < 175;
+    isAboveGround() {
+        if (this instanceof ThrowableObject) {
+            return true;
+        } else {
+            return this.y < 175;
+        }
     }
-  }
 
-  isColliding(mo) {
-    const thisX = this.x + this.offset.left;
-    const thisY = this.y + this.offset.top;
-    const thisWidth = this.width - this.offset.left - this.offset.right;
-    const thisHeight = this.height - this.offset.top - this.offset.bottom;
+    isColliding(mo) {
+        const thisX = this.x + this.offset.left;
+        const thisY = this.y + this.offset.top;
+        const thisWidth = this.width - this.offset.left - this.offset.right;
+        const thisHeight = this.height - this.offset.top - this.offset.bottom;
 
-    const moOffsetLeft = mo.offset ? mo.offset.left : 0;
-    const moOffsetRight = mo.offset ? mo.offset.right : 0;
-    const moOffsetTop = mo.offset ? mo.offset.top : 0;
-    const moOffsetBottom = mo.offset ? mo.offset.bottom : 0;
+        const moOffsetLeft = mo.offset ? mo.offset.left : 0;
+        const moOffsetRight = mo.offset ? mo.offset.right : 0;
+        const moOffsetTop = mo.offset ? mo.offset.top : 0;
+        const moOffsetBottom = mo.offset ? mo.offset.bottom : 0;
 
-    const moX = mo.x + moOffsetLeft;
-    const moY = mo.y + moOffsetTop;
-    const moWidth = mo.width - moOffsetLeft - moOffsetRight;
-    const moHeight = mo.height - moOffsetTop - moOffsetBottom;
+        const moX = mo.x + moOffsetLeft;
+        const moY = mo.y + moOffsetTop;
+        const moWidth = mo.width - moOffsetLeft - moOffsetRight;
+        const moHeight = mo.height - moOffsetTop - moOffsetBottom;
 
-    return (
-      thisX < moX + moWidth &&
-      thisY < moY + moHeight &&
-      thisX + thisWidth > moX &&
-      thisY + thisHeight > moY
-    );
-  }
-
-  hit() {
-    this.energy -= 5;
-    if (this.energy < 0) {
-      this.energy = 0;
-    } else {
-      this.lastHit = new Date().getTime();
+        return (
+            thisX < moX + moWidth &&
+            thisY < moY + moHeight &&
+            thisX + thisWidth > moX &&
+            thisY + thisHeight > moY
+        );
     }
-  }
 
-  isHurt() {
-    let timepassed = new Date().getTime() - this.lastHit;
-    timepassed = timepassed / 1000;
-    return timepassed < 0.3;
-  }
-
-  isDead() {
-    return this.energy == 0;
-  }
-
-  playAnimation(images) {
-    this.animationFrameSkip++;
-
-    if (this.animationFrameSkip >= this.animationSkipThreshold) {
-      let i = this.currentImage % images.length;
-      let path = images[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
-      this.animationFrameSkip = 0;
+    hit() {
+        this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
     }
-  }
 
-  moveRight() {
-    this.x += this.speed;
-    this.otherDirection = false;
-  }
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 0.3;
+    }
 
-  moveLeft() {
-    this.x -= this.speed;
-    this.otherDirection = true;
-  }
+    isDead() {
+        return this.energy == 0;
+    }
+
+    playAnimation(images, skipThreshold = 7) {
+        this.animationFrameSkip++;
+
+        if (this.animationFrameSkip >= skipThreshold) {
+            let i = this.currentImage % images.length;
+            let path = images[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+            this.animationFrameSkip = 0;
+        }
+    }
+
+    moveRight() {
+        this.x += this.speed;
+        this.otherDirection = false;
+    }
+
+    moveLeft() {
+        this.x -= this.speed;
+        this.otherDirection = true;
+    }
 }
--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,7 +1,31 @@
 class ThrowableObject extends MovableObject {
 
+    IMAGES_ROTATION = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ];
+
+    IMAGES_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ];
+
+    isSplashing = false;
+    splashAnimationFinished = false;
+    throwInterval = null;
+    splashInterval = null;
+    gravityInterval = null;
+
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_ROTATION);
+        this.loadImages(this.IMAGES_SPLASH);
         this.x = x;
         this.y = y;
         this.height = 80;
@@ -12,8 +36,30 @@ class ThrowableObject extends MovableObject {
     throw() {
         this.speedY = 30;
         this.applyGravity();
-        setInterval(() => {
-            this.x += 10;
+
+        this.throwInterval = setInterval(() => {
+            if (!this.isSplashing) {
+                this.x += 10;
+                this.playAnimation(this.IMAGES_ROTATION, 2);
+            }
         }, 25);
+    }
+
+    splash() {
+        this.isSplashing = true;
+        clearInterval(this.throwInterval);
+        clearInterval(this.gravityInterval);
+
+        this.currentImage = 0;
+        this.splashInterval = setInterval(() => {
+            if (this.currentImage < this.IMAGES_SPLASH.length) {
+                let path = this.IMAGES_SPLASH[this.currentImage];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            } else {
+                clearInterval(this.splashInterval);
+                this.splashAnimationFinished = true;
+            }
+        }, 80);
     }
 }
